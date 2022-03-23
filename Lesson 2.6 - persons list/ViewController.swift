70b58745317c54387457126f73684e19506116f9
AlexVerkhov <alexverkhov@gmail.com>
2022-03-23 22:43:22 +0700

    Initial Commit

---
+++ Lesson 2.6 - persons list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.6 - persons list
+//
+//  Created by Алексей Верховых on 23.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
